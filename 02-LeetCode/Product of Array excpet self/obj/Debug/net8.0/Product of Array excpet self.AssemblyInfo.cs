1236becfa279bbd2538294bea3983c4404304f74
Omar-Abo-Ziada <164497850+Omar-Abo-Ziada@users.noreply.github.com>
2024-11-04 18:48:31 +0200

    9 problems in sh2

--- 02-LeetCode/Product of Array excpet self/obj/Debug/net8.0/Product of Array excpet self.AssemblyInfo.cs
+++ 02-LeetCode/Product of Array excpet self/obj/Debug/net8.0/Product of Array excpet self.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Product of Array excpet self")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+335775c6e9c24205a4909524d76ab3e38fb00eb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9df7d794a23a2c2d43b36eed0c782a7ab9ae4217")]
 [assembly: System.Reflection.AssemblyProductAttribute("Product of Array excpet self")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Product of Array excpet self")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
