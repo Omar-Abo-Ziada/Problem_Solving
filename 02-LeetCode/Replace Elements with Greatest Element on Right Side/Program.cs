81e4f7c2d94b0716c170b3cd8fe5c0d0b0478b43
Omar-Abo-Ziada <164497850+Omar-Abo-Ziada@users.noreply.github.com>
2024-11-17 17:52:18 +0200

    clean

---
+++ 02-LeetCode/Replace Elements with Greatest Element on Right Side/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Replace_Elements_with_Greatest_Element_on_Right_Side
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = [17, 18, 5, 4, 6, 1];
+
+            arr = ReplaceElements(arr);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+        }
+
+        static int[] ReplaceElements(int[] arr)
+        {
+            if (arr.Length == 0 || arr is null) return [-1];
+
+            int[] result = new int[arr.Length];
+
+            int maxNumber = -1;
+
+            for (int i = arr.Length - 2; i >= 0; i--)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] > maxNumber)
+                    {
+                        maxNumber = arr[j];
+                    }
+                }
+
+                result[i] = maxNumber;
+            }
+
+            result[arr.Length - 1] = -1;
+
+            return result;
+        }
+    }
+}
--- 02-LeetCode/Squares of a Sorted Array/obj/Debug/net8.0/Squares of a Sorted Array.AssemblyInfo.cs
+++ 02-LeetCode/Squares of a Sorted Array/obj/Debug/net8.0/Squares of a Sorted Array.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Squares of a Sorted Array")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18a3ccb9c8d26a5b491cef3db9fbd25e963a712f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0e129a382ce416e908b773669a306313cf5f9e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Squares of a Sorted Array")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Squares of a Sorted Array")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
