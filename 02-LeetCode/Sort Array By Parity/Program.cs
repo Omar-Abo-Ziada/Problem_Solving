756e4128fc628d5e42a46c0f1ec56d10858d83c7
Omar-Abo-Ziada <164497850+Omar-Abo-Ziada@users.noreply.github.com>
2024-12-16 16:54:59 +0200

    Solved Sort Array By Parity

--- 02-LeetCode/Sort Array By Parity/Program.cs
+++ 02-LeetCode/Sort Array By Parity/Program.cs
@@ -5,7 +5,7 @@
         public static void Main(string[] args)
         {
             int[] nums = { 3, 1, 2, 4 };
-            
+
             int[] result = SortArrayByParity(nums);
 
             foreach (int item in result)
@@ -16,7 +16,29 @@
 
         public static int[] SortArrayByParity(int[] nums)
         {
+            int[] result = new int[nums.Length];
 
+            int j = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    result[j++] = nums[i];
+                }
+            }
+
+            int k = j;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 == 1)
+                {
+                    result[k++] = nums[i];
+                }
+            }
+
+            return result;
         }
     }
 }
