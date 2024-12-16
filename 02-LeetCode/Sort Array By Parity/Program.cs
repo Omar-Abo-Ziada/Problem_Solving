f01caab4fa709500f36a54a96580bc3dea48cbe0
Omar-Abo-Ziada <164497850+Omar-Abo-Ziada@users.noreply.github.com>
2024-12-16 16:26:39 +0200

    Apply updated .gitignore and remove tracked ignored files

---
+++ 02-LeetCode/Sort Array By Parity/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Sort_Array_By_Parity
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] nums = { 3, 1, 2, 4 };
+            
+            int[] result = SortArrayByParity(nums);
+
+            foreach (int item in result)
+            {
+                Console.Write($"{item}, ");
+            }
+        }
+
+        public static int[] SortArrayByParity(int[] nums)
+        {
+
+        }
+    }
+}
