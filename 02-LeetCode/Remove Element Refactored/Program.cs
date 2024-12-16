27ad58df361379a7c0a1c3880e6141edb3228b43
Omar-Abo-Ziada <164497850+Omar-Abo-Ziada@users.noreply.github.com>
2024-12-16 16:29:49 +0200

    Added Remove Element Refactores Proj

---
+++ 02-LeetCode/Remove Element Refactored/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Remove_Element_Refactored
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
