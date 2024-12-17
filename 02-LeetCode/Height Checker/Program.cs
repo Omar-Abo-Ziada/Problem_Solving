541121beb0173afe12759ff055ffc51359f6391b
Omar-Abo-Ziada <164497850+Omar-Abo-Ziada@users.noreply.github.com>
2024-12-17 18:31:32 +0200

    solved Height checker with Bubbe sort and Added General Diagram for my approaches to solve

---
+++ 02-LeetCode/Height Checker/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Globalization;
+
+namespace Height_Checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] heights = [1, 1, 4, 2, 1, 3];
+
+            Console.WriteLine(HeightChecker(heights));
+        }
+
+        public static int HeightChecker(int[] heights)
+        {
+            if (heights is null || heights.Length == 0) return 0;
+
+            int[] originalArr = new int[heights.Length];
+            for (int i = 0; i < heights.Length; i++)
+            {
+                originalArr[i] = heights[i];
+            }
+
+            bool swaped;
+            for (int i = 0; i < heights.Length - 1; i++)
+            {
+                swaped = false;
+
+                for (int j = 0; j < heights.Length - i - 1; j++)
+                {
+                    if (heights[j] > heights[j + 1])
+                    {
+                        int temp = heights[j];
+                        heights[j] = heights[j+1];
+                        heights[j+1] = temp;
+                        swaped = true;
+                    }
+                }
+
+                if (!swaped) break;
+            }
+
+            int wrongPlcaesCount = 0;
+            for(int i = 0; i < heights.Length; i++)
+            {
+                if (heights[i] != originalArr[i])
+                {
+                    wrongPlcaesCount++;
+                }
+            }
+
+            return wrongPlcaesCount;
+
+        }
+    }
+}
