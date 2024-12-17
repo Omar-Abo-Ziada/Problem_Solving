85c2b1f0c129f44cf0bf473b97aa71809b407459
Omar-Abo-Ziada <164497850+Omar-Abo-Ziada@users.noreply.github.com>
2024-12-17 17:46:40 +0200

    Solved Remove Element Refcatores => User w pointers approach opposite directions

--- 02-LeetCode/Remove Element Refactored/Program.cs
+++ 02-LeetCode/Remove Element Refactored/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] nums = { 2, 3, 2, 0,2 };
+
+            int k = RemoveElement(nums, 2);
+
+            for (int i = 0; i < k; i++)
+            {
+                Console.Write($"{nums[i]} ");
+            }
+        }
+
+        public static int RemoveElement(int[] nums, int val)
+        {
+            if (nums is null || nums.Length == 0) return 0;
+
+            int write = nums.Length - 1;
+            int valOccurance = 0;
+
+            for (int read = nums.Length - 1; read >= 0; read--)
+            {
+                if (nums[read] == val)
+                {
+                    int temp = nums[read];
+                    nums[read] = nums[write];
+                    nums[write] = temp;
+
+                    write--;
+
+                    valOccurance++;
+                }
+            }
+
+            int k = nums.Length - valOccurance;
+            return k;
         }
     }
 }
