06f8485b2f5f0c6978b694640d6fc8e48625348c
Omar-Abo-Ziada <164497850+Omar-Abo-Ziada@users.noreply.github.com>
2024-12-27 14:05:11 +0200

    Solved Find All the number Disappeared with O(n) time , O(1) space

---
+++ 02-LeetCode/Find All Numbers Disappeared in an Array/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Find_All_Numbers_Disappeared_in_an_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = [1,1];
+
+            List<int> result = FindDisappearedNumbers(nums).ToList();
+
+            foreach (var item in result)
+            {
+                Console.Write(item + " ");
+            }
+        }
+
+        static public IList<int> FindDisappearedNumbers(int[] nums)
+        {
+            if(nums == null || nums.Length == 0)
+            {
+                return new List<int>();
+            }
+
+            for(int i = 0; i < nums.Length ; i++)
+            {
+                int index = Math.Abs(nums[i]) - 1;
+
+                if (nums[index] > 0) // => not visited
+                {
+                    nums[index] = -nums[index]; // => mark as visited
+                }
+            }
+
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > 0)
+                {
+                   result.Add(i + 1);
+                }
+            }
+
+            return result;
+
+        }
+    }
+}
