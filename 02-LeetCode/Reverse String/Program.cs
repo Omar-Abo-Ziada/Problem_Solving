0ce1ab53203aa73b2288c8a83d66464de603dedc
Omar Ahmed <o.ahmed9847@gmail.com>
2025-03-18 22:36:27 +0200

    Feat : Solved Reverse String using two pointers => Time : O(n) , space : O(1)

---
+++ 02-LeetCode/Reverse String/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Reverse_String;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        char[] case1 = "hello".ToCharArray();
+        ReverseString(case1);
+        Console.WriteLine(case1); // Output: "olleh"
+    }
+
+    static public void ReverseString(char[] s)
+    {
+        if (s is null || !s.Any()) return;
+
+        int left = 0;
+        int right = s.Length - 1;
+
+        while(left < right)
+        {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
