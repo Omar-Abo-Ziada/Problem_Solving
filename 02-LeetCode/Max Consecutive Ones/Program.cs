4c9883afca41e5bf5f888f18f65296c111837db1
Omar Ahmed <o.ahmed9847@gmail.com>
2025-03-19 21:48:11 +0200

    Feat : Solved FindMaxConsecutiveOnes using 2 pointers => Time O (n) , sapce O(1)

---
+++ 02-LeetCode/Max Consecutive Ones/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Max_Consecutive_Ones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindMaxConsecutiveOnes([1, 1, 0, 1, 1, 1])); // 3
+            Console.WriteLine(FindMaxConsecutiveOnes([1, 0, 1, 1, 0, 1])); // 2
+        }
+
+        static public int FindMaxConsecutiveOnes(int[] nums)
+        {
+            int start = 0;
+            int end = 0;
+            int max = 0;
+
+            while (end < nums.Length)
+            {
+                if (nums[end] == 1)
+                {
+                    end++;
+                }
+                else
+                {
+                    max = Math.Max(max, end - start);
+                    start = end + 1;
+                    end++;
+                }
+            }
+
+            return Math.Max(max, end - start);
+        }
+    }
+}
