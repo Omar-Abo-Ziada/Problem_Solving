473bd2f9142eacc6a7d9932452b205b4d9007240
Omar Ahmed <o.ahmed9847@gmail.com>
2025-03-24 20:32:08 +0200

    Feat : Solved Reverse_Words_in_a_String => time O(n) and space O(n)

---
+++ 02-LeetCode/Reverse Words in a String/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Reverse_Words_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseWords(" The Sky Is blue "));
+            Console.WriteLine(ReverseWords("  hello world  "));
+            Console.WriteLine(ReverseWords("a good   example"));
+
+        }
+
+        static public string ReverseWords(string s)
+        {
+            List<string> stringsList = s.Trim()
+                                   .Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+                                   .ToList();
+
+            int left = 0;
+            int right = stringsList.Count - 1;
+
+            while (left < right)
+            {
+                string temp = stringsList[left];
+                stringsList[left] = stringsList[right];
+                stringsList[right] = temp;
+                left++;
+                right--;
+            }
+
+            return String.Join(' ',stringsList);
+        }
+    }
+}
