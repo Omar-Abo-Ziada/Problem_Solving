1709cf022e09dec98ab12cc2010f89e850e0e4e4
Omar Ahmed <o.ahmed9847@gmail.com>
2025-03-25 21:39:04 +0200

    Feat : ReverseWords => timeO(N) , Space O(N)

---
+++ 02-LeetCode/Reverse Words in a String III/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Reverse_Words_in_a_String_III
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string s = "Let's take LeetCode contest";
+            Console.WriteLine(ReverseWords(s)); //Output: "s'teL ekat edoCteeL tsetnoc"
+        }
+
+        static public string ReverseWords(string s)
+        {
+            List<string> words = s.Split(' ',
+                StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+                .ToList();
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                char[] charArray = words[i].ToCharArray();
+                Array.Reverse(charArray);
+                words[i] = new string(charArray); 
+            }
+
+            return String.Join(' ', words);
+        }
+    }
+}
