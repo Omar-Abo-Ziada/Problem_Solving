a4314ab1972f518a4986747ed8919faa3225aaa7
Omar Ahmed <o.ahmed9847@gmail.com>
2025-03-25 22:00:09 +0200

    Feat : Solved Remove Duplicatis in sorted arr => time => O(N)  , space => O(1) (Two-pointers technique)

---
+++ 02-LeetCode/Remove Duplicates from Sorted Array/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Remove_Duplicates_from_Sorted_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = [1, 1, 2];
+            int newLength = RemoveDuplicates(nums);
+            for (int i = 0; i < newLength; i++)
+            {
+                Console.WriteLine(nums[i] + " ");
+            }
+            Console.WriteLine();
+            int[] nums2 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+            int newLength2 = RemoveDuplicates(nums2);
+            for (int i = 0; i < newLength2; i++)
+            {
+                Console.WriteLine(nums2[i] + " ");
+            }
+        }
+
+        // time => O(N)  , space => O(1) (Two-pointers technique)
+        static public int RemoveDuplicates(int[] nums)
+        {
+            if (nums is null || nums.Length == 0)
+                return 0;
+
+            int writePointer = 1;
+            for (int readPointer = 1; readPointer < nums.Length; readPointer++)
+            {
+                if (nums[readPointer] != nums[readPointer - 1])
+                {
+                    nums[writePointer] = nums[readPointer];
+                    writePointer++;
+                }
+            }
+
+            return writePointer;
+        }
+    }
+}
