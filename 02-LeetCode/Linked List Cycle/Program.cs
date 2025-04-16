b89ed74291bf577490d523aef18cab2cae9d651e
Omar Ahmed <o.ahmed9847@gmail.com>
2025-04-16 19:46:53 +0200

    Solved Linked List Cycle using two pointers technique (fast , slow)

---
+++ 02-LeetCode/Linked List Cycle/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Linked_List_Cycle
+{
+    //Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x)
+        {
+            val = x;
+            next = null;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static public class Solution
+        {
+            public bool HasCycle(ListNode head)
+            {
+                if (head == null || head.next == null)
+                    return false;
+
+                ListNode slow = head;
+                ListNode fast = head.next;
+
+                while (fast != null && fast.next != null)
+                {
+                    if (fast == slow)
+                        return true;
+
+                    slow = slow.next;
+                    fast = fast.next.next;
+                }
+                return false;
+            }
+        }
+    }
+}
