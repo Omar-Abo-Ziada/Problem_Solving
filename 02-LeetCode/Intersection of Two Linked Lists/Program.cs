7e823d819fb1b04badade7aa698c2c24314624d3
Omar Ahmed <o.ahmed9847@gmail.com>
2025-04-25 22:55:43 +0300

    Solved Intersection_of_Two_Linked_Lists brute force sol using dictionary : time O(m+n) , space O(m+n)

---
+++ 02-LeetCode/Intersection of Two Linked Lists/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Intersection_of_Two_Linked_Lists
+{
+    //Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            if (headA == null || headB == null)
+                return null;
+
+            var dictionary = new Dictionary<ListNode, int>();
+
+            var current = headA;
+            bool isAdded = false;
+            while (current != null)
+            {
+                isAdded = dictionary.TryAdd(current, current.val);
+                if (!isAdded)
+                {
+                    return current;
+                }
+                current = current.next;
+            }
+
+            isAdded = false;
+            current = headB;
+            while (current != null)
+            {
+                isAdded = dictionary.TryAdd(current, current.val);
+                if (!isAdded)
+                {
+                    return current;
+                }
+                current = current.next;
+            }
+
+            return null;
+        }
+    }
+}
