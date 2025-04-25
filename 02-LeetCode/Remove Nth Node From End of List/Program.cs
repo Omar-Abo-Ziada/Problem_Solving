e5f7d0266959252438b43130adb8a6145d070bf5
Omar Ahmed <o.ahmed9847@gmail.com>
2025-04-26 00:16:21 +0300

    Solved Remove Nth Node From End of List : better solution using two pointers Maintain two pointers and update one with a delay of n steps passing 1 time only => time complexity: O(n), space complexity: O(1)

--- 02-LeetCode/Remove Nth Node From End of List/Program.cs
+++ 02-LeetCode/Remove Nth Node From End of List/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Remove_Nth_Node_From_End_of_List
+﻿using System.Reflection;
+
+namespace Remove_Nth_Node_From_End_of_List
 {
     //Definition for singly-linked list.
     public class ListNode
@@ -14,39 +16,78 @@
 
     internal class Program
     {
-        // first brute force solution passing 2 times => time complexity: O(n), space complexity: O(1)
+
+        // Better solution : (one pass ) Maintain two pointers and update one with a delay of n steps. => time complexity: O(n), space complexity: O(1) =>
         public ListNode RemoveNthFromEnd(ListNode head, int n)
         {
-            int length = 0;
-            ListNode current = head;
-            while(current != null)
+            if (head == null) return null;
+
+            int counter = 0;
+
+            ListNode fast = head;
+            ListNode slow = head;
+
+            // Move fast to n steps ahead
+            while (fast != null && counter < n)
             {
-                length++;
-                current = current.next;
+                fast = fast.next;
+                counter++;
             }
 
-            int index = length - n;
-            if (index == 0)
+            // If we need to remove the head node
+            if (fast == null)
             {
-                return head.next;
+                return head.next;  
             }
 
-            current = head;
-            int count = 1;
-            while(current != null)
+            while (fast.next != null)
             {
-                if (count == index)
-                {
-                    current.next = current.next.next;
-                    break;
-                }
-
-                count++;
-                current = current.next;
+                fast = fast.next;
+                slow = slow.next;
             }
+
+            // Remove the nth node from end
+            slow.next = slow.next.next;
 
             return head;
         }
+
+        // first brute force solution passing 2 times => time complexity: O(n), space complexity: O(1)
+        //public ListNode RemoveNthFromEnd(ListNode head, int n)
+        //{
+        //    int length = 0;
+        //    ListNode current = head;
+        //    while(current != null)
+        //    {
+        //        length++;
+        //        current = current.next;
+        //    }
+
+        //    int index = length - n;
+        //    if (index == 0)
+        //    {
+        //        return head.next;
+        //    }
+
+        //    current = head;
+        //    int count = 1;
+        //    while(current != null)
+        //    {
+        //        if (count == index)
+        //        {
+        //            current.next = current.next.next;
+        //            break;
+        //        }
+
+        //        count++;
+        //        current = current.next;
+        //    }
+
+        //    return head;
+        //}
+
+        // first brute force solution passing 2 times => time complexity: O(n), space complexity: O(1)
+
 
         static void Main(string[] args)
         {
