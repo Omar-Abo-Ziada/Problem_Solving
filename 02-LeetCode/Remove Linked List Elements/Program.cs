3dd42059df3c7f58466cdac8d688c2d5a8578f10
Omar Ahmed <o.ahmed9847@gmail.com>
2025-05-01 12:23:38 +0300

    Solved Remove Linked List Elements => Time O(n) , Space O(1)

---
+++ 02-LeetCode/Remove Linked List Elements/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Remove_Linked_List_Elements
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    internal class Program
+    {
+        public static ListNode RemoveElements(ListNode head, int val)
+        {
+            if (head == null)
+                return head;
+
+            ListNode current = head;
+
+            while (current != null)
+            {
+                if (current.val == val && current == head)
+                {
+                    head = current.next;
+                    current = current.next;
+                }
+                else if (current.next != null && current.next.val == val)
+                {
+                    current.next = current.next.next;
+                }
+                else
+                {
+                    current = current.next;
+                }
+            }
+
+            return head;
+
+        }
+        static void Main(string[] args)
+        {
+            //ListNode head = new ListNode(1);
+            //var newHead = RemoveElements(head, 2);
+
+            //ListNode head = new ListNode(1);
+            //var newHead = RemoveElements(head, 2);
+
+            ListNode head = new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(1, null))));
+            var newHead = RemoveElements(head, 2);
+        }
+    }
+}
