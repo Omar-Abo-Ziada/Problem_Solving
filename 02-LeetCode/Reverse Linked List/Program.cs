17ee3be04d1e6465dd1809cb50493e2bcd023b9f
Omar Ahmed <o.ahmed9847@gmail.com>
2025-05-01 11:23:56 +0300

    Solved Reverse Linked List : Time O(N) , Space O(1)

---
+++ 02-LeetCode/Reverse Linked List/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Reverse_Linked_List;
+
+//Definition for singly-linked list.
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+internal class Program
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode originalHead = head;
+
+        while (originalHead.next != null)
+        {
+            ListNode temp = originalHead.next;
+            originalHead.next = originalHead.next.next;
+            temp.next = head;
+            head = temp;
+        }
+
+        return head;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
