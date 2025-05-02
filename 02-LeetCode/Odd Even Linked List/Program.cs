c85bb91770ceb122539a6e39510408d6c99ebb30
Omar Ahmed <o.ahmed9847@gmail.com>
2025-05-02 17:52:11 +0300

    Solved Odd Even Linked List Sort : using two pointers technique => Time O(n) , sapce O(1)

---
+++ 02-LeetCode/Odd Even Linked List/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Odd_Even_Linked_List
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    internal class Program
+    {
+        public static ListNode OddEvenList(ListNode head)
+        {
+            if (head == null || head.next == null) 
+                return head;
+
+            ListNode odd = head;
+            ListNode even = head.next;
+
+            ListNode evenHead = head.next;
+
+            bool finshed = false;
+
+            while (even != null && even.next != null)
+            {
+                odd.next = even.next;
+                odd = even.next;
+
+                even.next = odd.next;
+                even = odd.next;
+            }
+
+            odd.next = evenHead;
+
+            return head;
+        }
+
+        static void Main(string[] args)
+        {
+            var head = new ListNode(1,
+                new ListNode(2,
+                     new ListNode(3,
+                       new ListNode(4,
+                            new ListNode(5,
+                                 new ListNode(6))))));
+
+            OddEvenList(head);
+        }
+    }
+}
