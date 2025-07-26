1b6584d547cf70e642853c9d4c1193618dd97a8d
Omar Ahmed <o.ahmed9847@gmail.com>
2025-07-26 21:38:51 +0300

    ContainsDuplicate Hashset => time O(1) , Space O(n)

---
+++ 02-LeetCode/Hashset Contains Duplicate/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Hashset_Contains_Duplicate;
+
+internal class Program
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        List<int> hashset = new List<int>();
+        foreach (int num in nums)
+        {
+            if (hashset.Contains(num))
+            {
+                return true;
+            }
+            hashset.Add(num);
+        }
+        return false;
+    }
+
+    static void Main(string[] args)
+    {
+    }
+}
