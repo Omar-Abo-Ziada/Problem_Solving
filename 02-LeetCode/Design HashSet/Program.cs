8788175edcd5c0bd6dfdcd9a6e75d94373616d55
Omar Ahmed <o.ahmed9847@gmail.com>
2025-07-26 21:16:54 +0300

    Simple hashset Design => time O(1) , Space O(n)

---
+++ 02-LeetCode/Design HashSet/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Design_HashSet;
+
+public class MyHashSet
+{
+    public bool[] Elements { get; set; }
+
+    public MyHashSet()
+    {
+        Elements = new bool[1_000_001];
+    }
+
+    public void Add(int key)
+    {
+        Elements[key] = true;
+    }
+
+    public void Remove(int key)
+    {
+        Elements[key] = false;
+    }
+
+    public bool Contains(int key)
+    {
+        return Elements[key];
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+    }
+}
