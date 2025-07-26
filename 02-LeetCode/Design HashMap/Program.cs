b4451a02f4289230d074d20cd88a8d019947cfaf
Omar Ahmed <o.ahmed9847@gmail.com>
2025-07-26 21:26:44 +0300

    Simple Hashmap => time O(1) , Space O(n)

---
+++ 02-LeetCode/Design HashMap/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Design_HashMap;
+
+public class MyHashMap
+{
+    private int[] mapArray;
+
+    public MyHashMap()
+    {
+        mapArray = new int[1000001];
+        Array.Fill(mapArray, -1);
+    }
+
+    public void Put(int key, int value)
+    {
+        mapArray[key] = value;
+    }
+
+    public int Get(int key)
+    {
+        return mapArray[key];
+
+    }
+
+    public void Remove(int key)
+    {
+        mapArray[key] = -1;
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+    }
+}
