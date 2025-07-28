e3f5bad4215b5ee14712d24fd917626a04873b62
Omar Ahmed <o.ahmed9847@gmail.com>
2025-07-28 21:11:37 +0300

    Single Number Using XOR Operator => time O(n) , space O(1)

---
+++ 02-LeetCode/Single Number/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Single_Number;
+
+internal class Program
+{
+    public static int SingleNumber(int[] nums)
+    {
+        int single = 0;
+
+        foreach (int num in nums)
+        {
+            single ^= num;
+        }
+
+        return single;
+
+    }
+    static void Main(string[] args)
+    {
+        SingleNumber([4, 1, 2, 1, 2]);
+    }
+}
