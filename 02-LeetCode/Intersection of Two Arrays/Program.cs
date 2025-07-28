a7bd1948b7a872d12d04cd47b9b0cd1bf31cc2a5
Omar Ahmed <o.ahmed9847@gmail.com>
2025-07-28 21:23:56 +0300

    Solved Intersection of Two Arrays using Hashet => time O(n) , Space O(n)

---
+++ 02-LeetCode/Intersection of Two Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Intersection_of_Two_Arrays;
+
+internal class Program
+{
+    public static int[] Intersection(int[] nums1, int[] nums2)
+    {
+        if ((nums1 is null || nums1.Length == 0)
+             && (nums2 is null || nums2.Length == 0))
+            return [];
+
+        HashSet<int> set1 = new (nums1);
+        HashSet<int> set2 = new(nums2);
+
+        List<int> result = new();
+
+        foreach(int num in set2)
+        {
+            if (set1.Contains(num))
+            {
+                result.Add(num);
+            }
+        }
+
+        return result.ToArray();
+    }
+
+    static void Main(string[] args)
+    {
+        int[] nums1 = { 1, 2, 2, 1 };
+        int[] nums2 = { 2, 2 };
+        Intersection(nums1, nums2);
+    }
+}
