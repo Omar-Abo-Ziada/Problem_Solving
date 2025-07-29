8608ad5524e4c7d67d4668c9ead08233a35e5ad7
Omar Ahmed <o.ahmed9847@gmail.com>
2025-07-29 21:14:53 +0300

    Solved Is Happy Number => time O(n) , space O(n)

---
+++ 02-LeetCode/Happy Number/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace Happy_Number;
+
+internal class Program
+{
+
+    public static bool IsHappy(int n)
+    {
+
+        HashSet<int> seen = new();
+
+        while (n != 1)
+        {
+            if (!seen.Contains(n))
+            {
+                seen.Add(n);
+                n = GetSquareDigitsSum(n);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+    public static int GetSquareDigitsSum(int number)
+    {
+        int sum = 0;
+        while (number != 0)
+        {
+            int digit = number % 10;
+            sum += digit * digit;
+            number /= 10;
+        }
+        return sum;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(IsHappy(2));
+    }
+}
