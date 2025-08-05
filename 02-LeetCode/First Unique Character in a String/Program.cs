0dd55dfb7e9bcd6bb241bb8df1d22874cb8d5ae3
Omar Ahmed <o.ahmed9847@gmail.com>
2025-08-05 21:30:27 +0300

    Solved First Unique Character in a String using HashMap => time O(n) , Space O(1) = bcause the dic size is const by max 26 lowercase letters.

---
+++ 02-LeetCode/First Unique Character in a String/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace First_Unique_Character_in_a_String;
+
+internal class Program
+{
+    public int FirstUniqChar(string s)
+    {
+        if (String.IsNullOrEmpty(s)) return -1;
+
+        Dictionary<char, int> charsMap = new(); // (key)char => (value)occurances count
+        for (int i = 0; i < s.Length; i++)
+        {
+            char c = s[i];
+            if (charsMap.ContainsKey(c))
+            {
+                charsMap[c]++;
+            }
+            else
+            {
+                charsMap.Add(c, 1);
+            }
+        }
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (charsMap[s[i]] == 1)
+                return i;
+        }
+
+        return -1; // No unique character found
+    }
+
+    static void Main(string[] args)
+    {
+    }
+}
