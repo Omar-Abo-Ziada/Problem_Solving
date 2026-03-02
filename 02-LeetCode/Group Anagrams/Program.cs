50cff75332b72250a044020e486a1faf07151f44
Omar Ahmed <o.ahmed9847@gmail.com>
2026-03-02 23:24:25 +0200

    Solved Group_Anagrams : Time O(n) , Space O(n)

---
+++ 02-LeetCode/Group Anagrams/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Group_Anagrams;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+         Input: strs = ["eat","tea","tan","ate","nat","bat"]
+         Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+        */
+
+        string[] strs = new[] { "eat", "tea", "tan", "ate", "nat", "bat" };
+        IList<IList<string>> result = GroupAnagrams(strs);
+        foreach (IList<string> group in result)
+        {
+            Console.WriteLine($"[{string.Join(", ", group)}]");
+        }
+    }
+    public static IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        Dictionary<string, List<string>> hashMap = [];
+
+        foreach (string word in strs)
+        {
+            // Convert to char array
+            char[] chars = word.ToCharArray();
+
+            // Sort characters
+            Array.Sort(chars);
+
+            // Create key
+            string key = new(chars);
+
+            // If key doesn't exist, create new list
+            if (!hashMap.ContainsKey(key))
+            {
+                hashMap[key] = [];
+            }
+
+            // Add original word
+            hashMap[key].Add(word);
+        }
+
+        return hashMap.Values
+                      .Select(list => (IList<string>)list)
+                      .ToList();
+    }
+
+}
